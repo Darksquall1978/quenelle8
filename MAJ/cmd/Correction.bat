8a3ff0264faec6702c1bb309092acaa99f8a8836
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2024-08-13 18:23:58 +0200

    Update Correction.bat

--- MAJ/cmd/Correction.bat
+++ MAJ/cmd/Correction.bat
@@ -2,24 +2,14 @@
 Color 0A & Mode con cols=120 lines=100
 batbox /c 0X06 /d  
 
-IF EXIST quenelle5 RMDIR /s /q quenelle5
+IF EXIST quenelle5 RMDIR /s /q quenelle8
 timeout /t 2 /nobreak > nul
-git clone https://github.com/Darksquall1978/quenelle5.git 
+git clone https://github.com/Darksquall1978/quenelle8.git 
 batbox /c 0X02 /d 
 cd..
 
-::@echo Optimisation du code
 
-::IF EXIST clone.bat del clone.bat
-::IF EXIST git.bat del git.bat
-::IF EXIST git.exe del git.exe
-::IF EXIST "Maj GIT".bat del "Maj GIT".bat
-::IF EXIST modif.bat del modif.bat
-::IF EXIST Reparer.bat del Reparer.bat
-::IF EXIST Reparerfin.bat del Reparerfin.bat
-::IF EXIST start-ssh-pageant.cmd del start-ssh-pageant.cmd
-::IF EXIST start-ssh-agent.cmd del start-ssh-agent.cmd
-::IF EXIST reparation.bat del reparation.bat
+
 
 cd..
 @echo off
@@ -27,7 +17,7 @@ cd..
 @echo off
 IF EXIST .git RMDIR /s /q .git
 
-@echo MAJ du --21/04/2023-- 17H15
+@echo MAJ du --13/08/2024-- 18H23
 ::pause
 
 timeout /t 1 /nobreak > nul
@@ -36,14 +26,12 @@ timeout /t 1 /nobreak > nul
 robocopy maj\cmd\quenelle5\.git .git /mt /z /e
 timeout /t 1 /nobreak > nul
 
-robocopy maj\cmd\quenelle5 ./
+robocopy maj\cmd\quenelle8 ./
 
 
 
 @echo off
-IF EXIST maj\cmd\quenelle5 RMDIR /s /q maj\cmd\quenelle5
-
-::start maj\cmd\reparerfin.bat
+IF EXIST maj\cmd\quenelle5 RMDIR /s /q maj\cmd\quenelle8
 
 start launcher.exe
 exit
