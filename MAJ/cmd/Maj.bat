446fb10c24d4c01edb805aa6ac0b64ac9c53be63
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2024-08-13 18:24:36 +0200

    Update Maj.bat

--- MAJ/cmd/Maj.bat
+++ MAJ/cmd/Maj.bat
@@ -1,12 +1,9 @@
 @echo off
 Color 0e & Mode con cols=100 lines=40
 
-IF EXIST quenelle5 RMDIR /s /q quenelle5
+IF EXIST quenelle5 RMDIR /s /q quenelle8
 IF EXIST "Maj GIT".bat del "Maj GIT".bat
-IF EXIST reparation.bat del reparation.bat
-IF EXIST modif.bat del modif.bat
-IF EXIST Reparer.bat del Reparer.bat
-IF EXIST Reparerfin.bat del Reparerfin.bat
+
 IF EXIST clone.bat del clone.bat
 
 git pull
