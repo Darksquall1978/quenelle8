0b443b7cc057c96500fdff1929edfc1e0c70787b
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2024-08-18 11:37:50 +0200

    Update git.bat

--- MAJ/cmd/git.bat
+++ MAJ/cmd/git.bat
@@ -3,7 +3,7 @@
 
 @echo off
 Color 0A & Mode con cols=90 lines=60
-IF EXIST maj\cmd\quenelle8 RMDIR /s /q quenelle8
+IF EXIST quenelle8 RMDIR /s /q quenelle8
 git clone https://github.com/Darksquall1978/quenelle8.git 
 cd..
 cd..
