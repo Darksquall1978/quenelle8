68b621ca5845ff088ecdf603d5a8a7c1e5d15d86
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2024-08-18 11:17:38 +0200

    Add files via upload

---
+++ MAJ/cmd/git.bat
@@ -0,0 +1,23 @@
+
+
+
+@echo off
+Color 0A & Mode con cols=90 lines=60
+git clone https://github.com/Darksquall1978/quenelle8.git 
+cd..
+cd..
+dir
+
+ @echo  Copie des fichiers source
+robocopy maj\cmd\quenelle8\.git .git /mt /z /e
+timeout /t 1 /nobreak > nul
+
+robocopy maj\cmd\quenelle8 ./ /mt /z /e
+
+::pause
+
+@echo off
+IF EXIST maj\cmd\quenelle8 RMDIR /s /q maj\cmd\quenelle8
+exit
+
+
